cd61259449f2ba6e42d5f52c4c76337e3918bc9b
jigjagboi45 <61517778+jigjagboi45@users.noreply.github.com>
2025-02-24 13:00:41 +0100

    Delete vvlib_root directory

--- vvlib_root/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name = "vvlib",
-    packages = find_packages(include=["vvlib"]),
-    version = "0.0.1",
-    description = "Random functions",
-    author = "Vojtech Velebny"
-)
