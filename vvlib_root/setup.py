bbfb0c188ebe79ba163cfc252d40daa64a78f838
jigjagboi45 <61517778+jigjagboi45@users.noreply.github.com>
2025-02-24 12:59:51 +0100

    Add files via upload

---
+++ vvlib_root/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "vvlib",
+    packages = find_packages(include=["vvlib"]),
+    version = "0.0.1",
+    description = "Random functions",
+    author = "Vojtech Velebny"
+)
